8eaac69174fe26ebb7aab7be4e2b79805206d8cc
santhosh380 <mvsanthosh108@gmail.com>
2026-03-09 13:58:49 +0530

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 public class WordCount {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence:");
+
         String sentence = sc.nextLine();
 
-        String[] words = sentence.trim().split("\\s+");
-        System.out.println("Word count: " + words.length);
+        String upper = sentence.toUpperCase();
+
+        System.out.println("Uppercase sentence: " + upper);
+
+        sc.close();
     }
 }
